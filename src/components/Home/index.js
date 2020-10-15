15e9e58e81ca79cb37bbc0f6379e116cebaecdc5
vcaruso <vinnycar0923@gmail.com>
2020-10-14 18:41:47 -0700

    home page - needs content!

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,15 +4,9 @@ function Home() {
 
     return (
         <div className="about-section">
-            {/* <img
-                src={require(`../../assets/images/pro.jpg`)}
-                alt='Vincent Caruso on the beach'
-                key='pro-pic'
-            /> */}
             <div id="bio-wrapper">
-                <p id="bio-line-1">Hello, I'm Vince!</p>
-                <p className="bio-body">I'm a full-stack web developer.</p>
-                <p className="bio-body">Have a look at some projects I've worked on.</p>
+                <p id="bio-line-1">Hello, I'm Jared Nast!</p>
+                <p className="bio-body">I'm here to help you meet all your energy needs.</p>
                 <p className="bio-body">Feel free to reach out!</p>
             </div>
         </div>
