8e61096a5b51d8cf1514486793d98d0d63e66c30
vcaruso <vinnycar0923@gmail.com>
2020-10-20 18:21:07 -0700

    learn component

--- src/components/Learn/index.js
+++ src/components/Learn/index.js
@@ -5,8 +5,12 @@ function Learn() {
     return (
         <div className="learn-wrapper">
             <p className="learn-text">
-                Coming Soon!
+                Want to learn more about solar energy and how much money it could save you? Have a look at the slideshow below!
             </p>
+
+            <div className="learn-slideshow-wrapper">
+                <iframe className="learn-slideshow" src="https://docs.google.com/presentation/d/e/2PACX-1vQMLur8A9e3cgJYGRK8-QWHPerU5Z3O86i7OglY_mUKTtYzLzxjQdxFi7Nmo55EMrAHMMmz5x6bVqey/embed?start=false&loop=true&delayms=60000" frameborder="0" width="960" height="569" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
+            </div>
         </div>
     );
 }
